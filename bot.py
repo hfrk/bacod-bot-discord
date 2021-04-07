9f59d1d2ac119375cac715c4a8f442743f551caf
gldnpz17 <firdausbismasuryakusuma@gmail.com>
2021-04-07 20:33:15 +0700

    Test change.

--- bot.py
+++ bot.py
@@ -14,8 +14,8 @@ async def on_message(message):
     if message.author == client.user:
         return
     
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('hello'):
+        await message.channel.send('world')
 
 print('Bot started.')
 client.run(os.environ['DISCORD_TOKEN'])
